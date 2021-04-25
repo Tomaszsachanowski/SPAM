e6b9be57bf828f51cef6fd6ee4e7372e76d31515
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2021-04-25 21:51:40 +0200

    utoworzeni list krajów i recenzji.

--- read_data.py
+++ read_data.py
@@ -4,5 +4,21 @@ import pandas as pd
 raw_data = pd.read_csv('data/test_data.csv')
 
 # Pozyskanie dnaych o kraju i recenzji 
-df_country_reviews = raw_data[['country', 'description']]
+df = raw_data[['country', 'description']]
+# Połaczenie recenzji ze wspolnego kraju.
+df_country_reviews = df.groupby(['country'])['description'].apply(lambda x: ' '.join(x.astype(str))).reset_index()
 print(df_country_reviews)
+
+# Sprawdzam czy recenzja dla US jest dobrze połączona.
+US_reviews = df_country_reviews.loc[df_country_reviews['country'] == 'US']
+print(US_reviews)
+rewiew = US_reviews.iat[0, 1]
+print(rewiew)
+
+# Pozyskanie nazw krajów.
+country = list(df_country_reviews['country'])
+# Pozyskanie recenzji dla krajów
+reviews = list(df_country_reviews['description'])
+
+print(country)
+print(reviews[0])
