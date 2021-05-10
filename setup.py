25a2033cf53dc8ccf874a1df5756a26d5963d7e3
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2021-05-11 00:10:55 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+
+    name="SPAM",
+    version="0.1",
+    description="SPAM algoritm",
+    author=['Gabriela Ossowska', 'Tomasz Sachanowski'],
+    packages=setuptools.find_packages(),
+    install_requires=['numpy','dataframe', 'click']
+)
