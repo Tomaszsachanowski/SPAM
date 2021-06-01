868f1d9749360d554499d1bc183f5aa4961f41bf
Gabriela Ossowska <gab.ossowska@gmail.com>
2021-06-01 15:56:55 +0200

    tt

--- cli_spam.py
+++ cli_spam.py
@@ -7,6 +7,7 @@ from bitmap import generate_words_bitmaps
 from spam import SPAM, translate_patterns
 from config import Config
 from tweet_api import TweetApi
+import cmap
 
 
 TEST = Config.TEST
@@ -34,7 +35,10 @@ def start(minsup, data_path):
     sequences = DataSequence.data_sequence_factory(
         customers=TEST['customers'], texts=TEST['texts'], path=data_path)
     bitmaps_for_words_ids = generate_words_bitmaps(sequences)
-    spam_alg = SPAM(minsup, bitmaps_for_words_ids)
+    seq_list = cmap.transform_sequences_into_lists(sequences, len(sequences[0].get_cids()))
+    cmap_i = cmap.CMAP(seq_list, minsup*len(seq_list)).build_cmap_i()
+    cmap_s = cmap.CMAP(seq_list, minsup*len(seq_list)).build_cmap_s()
+    spam_alg = SPAM(minsup, bitmaps_for_words_ids, cmap_i, cmap_s)
     frequent_patterns = spam_alg.spam()
 
     with open("frequent_patterns", 'w') as file: 
@@ -47,10 +51,22 @@ def start(minsup, data_path):
     short_help="Get tweets from tweeter")
 @click.argument('search_word', type=str, default=TWEET['search_word'])
 @click.argument('items', type=int, default=TWEET['items'])
-def get_tweets(search_word, items):
+@click.argument('minsup', nargs=1, required=True, type=float)
+def get_tweets(minsup, search_word, items):
     data = TweetApi.collect_tweets(search_word=search_word, items=items)
     TweetApi.save_collected_tweets(data)
+    sequences = DataSequence.data_sequence_factory(
+        customers=TEST['customers'], texts=TEST['texts'], path=TWEET['output'])
+    bitmaps_for_words_ids = generate_words_bitmaps(sequences)
+    seq_list = cmap.transform_sequences_into_lists(sequences, len(sequences[0].get_cids()))
+    cmap_i = cmap.CMAP(seq_list, minsup*len(seq_list)).build_cmap_i()
+    cmap_s = cmap.CMAP(seq_list, minsup*len(seq_list)).build_cmap_s()
+    spam_alg = SPAM(minsup, bitmaps_for_words_ids, cmap_i, cmap_s)
+    frequent_patterns = spam_alg.spam()
 
+    with open("frequent_patterns_tt", 'w') as file: 
+        for pattern in translate_patterns(frequent_patterns):
+            file.write(str(pattern) + '\n')
 
 # Przykład użycia python cli_spam start 2 /data/test_simple.csv
 # Przykład użycia python cli_spam get-tweets /#Coivd-19 10
